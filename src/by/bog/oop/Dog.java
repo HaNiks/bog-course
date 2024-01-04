37f6ff9c446fcd9ba9204be2405384357fa864e4
Vadim1233215 <elena.bogumil@mail.ru>
2024-01-04 20:15:03 +0300

    Add OOP

--- src/by/bog/oop/Dog.java
+++ src/by/bog/oop/Dog.java
@@ -1,22 +1,21 @@
 package by.bog.oop;
 
-/* Написать программу, которая выводит в консоль объект класса dog (содержащий кличку и возраст) */
 public class Dog {
+
     private String name;
     private int age;
-
 
     public Dog(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    public Dog(String name) {
-        this.name = name;
+    public String getName() {
+        return name;
     }
 
-    public Dog(int age) {
-        this.age = age;
+    public int getAge() {
+        return age;
     }
 
     @Override
@@ -27,3 +26,4 @@ public class Dog {
                 '}';
     }
 }
+
--- src/by/bog/oop/Main.java
+++ src/by/bog/oop/Main.java
@@ -1,8 +1,16 @@
 package by.bog.oop;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-       Dog myDog = new Dog("Барсик" , 4);
-        System.out.println(myDog);
+        Dog[] dogs = new Dog[3];
+        dogs[0] = new Dog("Барсик", 3);
+        dogs[1] = new Dog("Шарик", 5);
+        dogs[2] = new Dog("Рекс", 2);
+
+        System.out.println(Arrays.toString(dogs));
+
     }
 }
+
