a2b5f181282a1cc6e8576260aedc415c4c89391f
Vadim1233215 <elena.bogumil@mail.ru>
2024-01-06 10:37:52 +0300

    Add OOP add

--- src/by/bog/oop/Main.java
+++ src/by/bog/oop/Main.java
@@ -1,16 +1,32 @@
 package by.bog.oop;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Task1();
+        Task2();
+    }
+
+    public static void Task1() {
         Dog[] dogs = new Dog[3];
         dogs[0] = new Dog("Барсик", 3);
         dogs[1] = new Dog("Шарик", 5);
         dogs[2] = new Dog("Рекс", 2);
 
         System.out.println(Arrays.toString(dogs));
+    }
 
+    public static void Task2() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите кличку");
+        String name = sc.nextLine();
+        System.out.println("Введитье возраст");
+        int age = sc.nextInt();
+        Dog myDog = new Dog(name, age);
+        System.out.println(myDog.getName());
+        System.out.println(myDog.getAge());
     }
 }
 
